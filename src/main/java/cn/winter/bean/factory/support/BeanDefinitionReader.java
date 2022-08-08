25e9761153d9a2047b6172710d78883d5e3d2dc0
pengxuanyu0831 <50193069+pengxuanyu0831@users.noreply.github.com>
2022-08-08 23:09:06 +0800

    资源加载器实现+XML解析注册

--- src/main/java/cn/winter/bean/factory/support/BeanDefinitionReader.java
+++ src/main/java/cn/winter/bean/factory/support/BeanDefinitionReader.java
@@ -6,15 +6,17 @@ import cn.winter.core.io.Resource;
 import cn.winter.core.io.ResourceLoader;
 import cn.winter.core.support.BeanDefinitionRegistry;
 
+import java.io.IOException;
+
 public interface BeanDefinitionReader {
     BeanDefinitionRegistry getRegistry();
 
     ResourceLoader getResourceLoader();
 
-    void loadBeanDefinitions(Resource resource) throws BeansException;
+    void loadBeanDefinitions(Resource resource) throws BeansException, IOException;
 
-    void loadBeanDefinitions(Resource... resources) throws BeansException;
+    void loadBeanDefinitions(Resource... resources) throws BeansException, IOException;
 
-    void loadBeanDefinitions(String location) throws BeansException;
+    void loadBeanDefinitions(String location) throws BeansException, IOException;
 
 }
