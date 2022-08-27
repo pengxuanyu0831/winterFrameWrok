1e8ff28c4b54250e76ded6b0202f4a299432175e
pengxuanyu0831 <pengxuanyus@gmail.com>
2022-08-27 23:04:57 +0800

    引入PostProcess机制-->提供接口修改BeanDefinition

---
+++ src/main/java/cn/winter/bean/factory/config/BeanFactoryPostProcessor.java
@@ -0,0 +1,12 @@
+package cn.winter.bean.factory.config;
+
+/**
+ * @program spring-core
+ * @description: 在所有的BeanDefinition 加载完成后，实例化Bean对象之前，提供修改BeanDefinition的接口
+ * @author: pengxuanyu
+ * @create: 2022/08/27 23:00
+ * @version: 1.0
+ */
+public interface BeanFactoryPostProcessor {
+    // TODO
+}
