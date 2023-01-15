72233ea7e015d7fd0de7a2f7dc4ba036139f901d
pengxuanyu <pengxuanyus@gmail.com>
2023-01-15 21:46:16 +0800

    手写Spring 加入Bean对象作用域的概念

--- src/main/java/cn/winter/bean/factory/support/AbstractBeanFactory.java
+++ src/main/java/cn/winter/bean/factory/support/AbstractBeanFactory.java
@@ -3,6 +3,7 @@ package cn.winter.bean.factory.support;
 import cn.winter.bean.BeansException;
 import cn.winter.bean.factory.BeanFactory;
 import cn.winter.bean.factory.ConfigurableBeanFactory;
+import cn.winter.bean.factory.FactoryBean;
 import cn.winter.bean.factory.config.BeanDefinition;
 import cn.winter.bean.factory.config.BeanPostProcessor;
 import cn.winter.util.ClassUtils;
@@ -16,7 +17,10 @@ import java.util.List;
  * @author: pengxuanyu
  * @create: 2021/12/01 22:40
  */
-public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements ConfigurableBeanFactory {
+public abstract class AbstractBeanFactory extends FactoryBeanRegistrySupport implements ConfigurableBeanFactory { // 原来继承的 DefaultSingletonBeanRegistry，修改
+//    为继承 FactoryBeanRegistrySupport。因为需要扩展出创建 FactoryBean 对象的能
+//    力，所以这就想一个链条服务上，截出一个段来处理额外的服务，并把链条再链接
+//            上
     /** BeanPostProcessors to apply in createBean */
     private final List<BeanPostProcessor> beanPostProcessors = new ArrayList<BeanPostProcessor>();
 
@@ -36,12 +40,24 @@ public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry i
     private Object doGetBean(String beanName, Object... args) {
         Object bean = getSingleton(beanName);
         if (null != bean) {
-            return bean;
-        } else {
-            // 这里没有实现获取方法，而是定义调用过程，以及提供抽象方法
-            BeanDefinition beanDefinition = getBeanDefinition(beanName);
-            return this.createBean(beanName, beanDefinition, args);
+            return getObjectForBeanInstance(bean, beanName);
         }
+        // 这里没有实现获取方法，而是定义调用过程，以及提供抽象方法
+        BeanDefinition beanDefinition = getBeanDefinition(beanName);
+        return this.createBean(beanName, beanDefinition, args);
+    }
+
+    private Object getObjectForBeanInstance(Object beanInstance, String beanName) {
+        if (!(beanInstance instanceof FactoryBean)) {
+            return beanInstance;
+        }
+
+        Object object = getCacheObjectForFactoryBean(beanName);
+        if (object == null) {
+            FactoryBean<?> factoryBean = (FactoryBean<?>) beanInstance;
+            object = getObjectFromFactoryBean(factoryBean, beanName);
+        }
+        return object;
     }
 
     protected abstract BeanDefinition getBeanDefinition(String name);
--- src/main/java/cn/winter/bean/factory/support/FactoryBeanRegistrySupport.java
+++ src/main/java/cn/winter/bean/factory/support/FactoryBeanRegistrySupport.java
@@ -1,5 +1,8 @@
 package cn.winter.bean.factory.support;
 
+import cn.winter.bean.BeansException;
+import cn.winter.bean.factory.FactoryBean;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -15,5 +18,27 @@ public abstract class FactoryBeanRegistrySupport extends DefaultSingletonBeanReg
     protected Object getCacheObjectForFactoryBean(String beanName) {
         Object o = this.factoryBeanObjectCache.get(beanName);
         return o != NULL_OBJECT ? o : null;
+    }
+
+    protected Object getObjectFromFactoryBean(FactoryBean factoryBean, String beanName) {
+        if (factoryBean.isSingleton()) {
+            Object o = this.factoryBeanObjectCache.get(beanName);
+            if (o == null) {
+                o = doGetObjectFromFactoryBean(factoryBean, beanName);
+                this.factoryBeanObjectCache.put(beanName, o == null ? NULL_OBJECT : o);
+            }
+            return o == null ? NULL_OBJECT : o;
+        } else {
+            return doGetObjectFromFactoryBean(factoryBean, beanName);
+        }
+    }
+
+
+    private Object doGetObjectFromFactoryBean(final FactoryBean factoryBean, final String beanName) {
+        try {
+            return factoryBean.getObject();
+        } catch (Exception e) {
+            throw new BeansException("FactoryBean throw exception on object [" + beanName + "] creation");
+        }
     }
 }
